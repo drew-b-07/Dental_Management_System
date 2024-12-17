e317a6b5cded23102f79f02f90a144ac310776bb
KyleZablan <2022311292@dhvsu.edu.ph>
2024-12-17 23:35:53 +0800

    ZABLAN

--- src/js/popup-logout.js
+++
@@ -1,5 +0,0 @@
-function logout() {
-    if(confirm("Are you sure you want to logout?")) {
-        location.href = "../../dashboard_user/user/authentication_user/user-class.php?user_signout";
-    }
-}
